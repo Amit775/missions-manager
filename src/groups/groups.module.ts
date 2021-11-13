780f5db18c2896743dff9c6a020b003da33b3c1c
Amit775 <38419204+Amit775@users.noreply.github.com>
2021-11-13 20:04:41 +0200

    add services

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GroupsController } from './groups.controller';
+import { GroupsService } from './groups.service';
 
 @Module({
-  controllers: [GroupsController]
+  controllers: [GroupsController],
+  providers: [GroupsService]
 })
 export class GroupsModule {}
--- src/missions/missions.module.ts
+++ src/missions/missions.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MissionsController } from './missions.controller';
+import { MissionsService } from './missions.service';
 
 @Module({
-    controllers: [MissionsController]
+    controllers: [MissionsController],
+    providers: [MissionsService]
 })
 export class MissionsModule {}
